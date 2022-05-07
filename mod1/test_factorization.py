15b975b936bb2448c55d451481c02e34fe61b8ea
Dawid <klukowski_dawid@o2.pl>
2022-05-07 17:15:30 +0100

    Test case added - number has multiple different factors.

--- mod1/test_factorization.py
+++ mod1/test_factorization.py
@@ -23,9 +23,13 @@ def test_number_with_same_factors():
     result = prime_factors(32)
     assert result == [2, 2, 2, 2, 2], f'Expected [2, 2, 2, 2, 2], got {result}'
 
+def test_number_with_different_factors():
+    result = prime_factors(36)
+    assert result == [2, 2, 3, 3], f'Expected [2, 2, 3, 3], got {result}'
+
 if __name__ == '__main__':
     for test in (test_import_factorization, test_result_is_a_list, test_number_is_a_prime, test_number_is_not_a_prime,
-                 test_number_with_same_factors):
+                 test_number_with_same_factors, test_number_with_different_factors):
         print(f'{test.__name__}: ', end=' ')
         try:
             test()
