23b1c1fa52656b6c94f801beb7ba7abdd8be5131
Dawid <klukowski_dawid@o2.pl>
2022-05-07 17:52:14 +0100

    Test cases added - non-positive numbers and 1

--- mod1/test_factorization.py
+++ mod1/test_factorization.py
@@ -31,9 +31,31 @@ def test_big_number():
     result = prime_factors(3958159172)
     assert result == [2, 2, 11, 2347, 38329], f'Expected [2, 2, 11, 2347, 38329], got {result}'
 
+def test_negative_number():
+    try:
+        result = prime_factors(-100)
+        assert False, f'Expected ValueError, got {result}'
+    except ValueError:
+        pass
+
+def test_zero():
+    try:
+        result = prime_factors(0)
+        assert False, f'Expected ValueError, got {result}'
+    except ValueError:
+        pass
+
+def test_one():
+    try:
+        result = prime_factors(1)
+        assert False, f'Expected ValueError, got {result}'
+    except ValueError:
+        pass
+
 if __name__ == '__main__':
     for test in (test_import_factorization, test_result_is_a_list, test_number_is_a_prime, test_number_is_not_a_prime,
-                 test_number_with_same_factors, test_number_with_different_factors, test_big_number):
+                 test_number_with_same_factors, test_number_with_different_factors, test_big_number, test_negative_number,
+                 test_zero, test_one):
         print(f'{test.__name__}: ', end=' ')
         try:
             test()
