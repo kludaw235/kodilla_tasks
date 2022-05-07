71bd35549b0c58ca4d8c2892d1fa201a2ec78795
Dawid <klukowski_dawid@o2.pl>
2022-05-07 15:32:35 +0100

    Test case added - number has multiple same factors.

--- mod1/test_factorization.py
+++ mod1/test_factorization.py
@@ -19,8 +19,13 @@ def test_number_is_not_a_prime():
     result = prime_factors(12)
     assert result != [12], f'Expected not [12], got {result}'
 
+def test_number_with_same_factors():
+    result = prime_factors(32)
+    assert result == [2, 2, 2, 2, 2], f'Expected [2, 2, 2, 2, 2], got {result}'
+
 if __name__ == '__main__':
-    for test in (test_import_factorization, test_result_is_a_list, test_number_is_a_prime, test_number_is_not_a_prime):
+    for test in (test_import_factorization, test_result_is_a_list, test_number_is_a_prime, test_number_is_not_a_prime,
+                 test_number_with_same_factors):
         print(f'{test.__name__}: ', end=' ')
         try:
             test()
