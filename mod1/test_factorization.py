f4bcbea7bfb4eb953eea6127462a8c8e58581d50
Dawid <klukowski_dawid@o2.pl>
2022-05-07 13:42:50 +0100

    Test case added - number is/is not a prime.

--- mod1/test_factorization.py
+++ mod1/test_factorization.py
@@ -11,9 +11,16 @@ def test_result_is_a_list():
     result = prime_factors(2)
     assert type(result) is list, 'Result is not a list'
 
+def test_number_is_a_prime():
+    result = prime_factors(11)
+    assert result == [11], f'Expected [11], got {result}'
+
+def test_number_is_not_a_prime():
+    result = prime_factors(12)
+    assert result != [12], f'Expected not [12], got {result}'
 
 if __name__ == '__main__':
-    for test in (test_import_factorization, test_result_is_a_list):
+    for test in (test_import_factorization, test_result_is_a_list, test_number_is_a_prime, test_number_is_not_a_prime):
         print(f'{test.__name__}: ', end=' ')
         try:
             test()
