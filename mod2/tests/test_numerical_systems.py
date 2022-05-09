54d2e20a0b8043a445beca86800d68243db172e3
Dawid <klukowski_dawid@o2.pl>
2022-05-09 18:54:40 +0100

    Test case added - ROM to DEC - target is int

--- mod2/tests/test_numerical_systems.py
+++ mod2/tests/test_numerical_systems.py
@@ -1,8 +1,15 @@
+from mod2.numerical_systems import NumericalSystemsConverter as nsv
+import pytest
+
 
 
 def test_import_NumericalSystemsConverter():
     try:
         from mod2.numerical_systems import NumericalSystemsConverter
-        assert callable(NumericalSystemsConverter), 'prime_factors is not callable'
+        assert callable(NumericalSystemsConverter), 'NumericalSystemsConverter is not callable'
     except ImportError as error:
         assert False, error
+
+
+def test_rom_to_dec_target_int():
+    assert isinstance(nsv('ROM', 'DEC', 'IV').target_number, int)
