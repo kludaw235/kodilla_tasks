1535c34caf64f538eecea73c9045c2f7ce4209e6
Dawid <klukowski_dawid@o2.pl>
2022-05-09 18:54:51 +0100

    Test passed - ROM to DEC - target is int

--- mod2/numerical_systems.py
+++ mod2/numerical_systems.py
@@ -1,3 +1,11 @@
 
 class NumericalSystemsConverter:
-    pass
+    def __init__(self, src_system, target_system, number):
+        self.number = number
+        self.target_number = None
+
+        if src_system == 'ROM' and target_system == 'DEC':
+           self.rom_to_dec()
+
+    def rom_to_dec(self):
+        self.target_number = 4
