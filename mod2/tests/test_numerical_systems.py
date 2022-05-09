cc1af0ba58b536116c14ec6131adbb2a16fccf1f
Dawid <klukowski_dawid@o2.pl>
2022-05-09 20:28:13 +0100

    Test cases added - ROM to DEC - great numbers <5000

--- mod2/tests/test_numerical_systems.py
+++ mod2/tests/test_numerical_systems.py
@@ -32,3 +32,20 @@ class TestRomToDecSmallNums:
 
     def test_5(self):
         assert nsv('ROM', 'DEC', 'XXXVI').target_number == 36
+
+class TestRomToDecGreatNums:
+    def test_1(self):
+        assert nsv('ROM', 'DEC', 'MDCLXV').target_number == 1665
+
+    def test_2(self):
+        assert nsv('ROM', 'DEC', 'MMMMDCCCXXXIX').target_number == 4839
+
+    def test_3(self):
+        assert nsv('ROM', 'DEC', 'MMMDCCCLXXXVIII').target_number == 3888
+
+    def test_4(self):
+        assert nsv('ROM', 'DEC', 'MMDMCCLCXXVXIII').target_number == 2778
+
+    def test_5(self):
+        assert nsv('ROM', 'DEC', 'CMXCIX').target_number == 999
+
