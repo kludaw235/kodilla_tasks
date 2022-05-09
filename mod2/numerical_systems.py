e308b175b4a75561246a99a75cab143821fdecc8
Dawid <klukowski_dawid@o2.pl>
2022-05-09 20:07:18 +0100

    Tests passed - ROM to DEC - small numbers <40

--- mod2/numerical_systems.py
+++ mod2/numerical_systems.py
@@ -1,3 +1,7 @@
+rom_to_dec_dict = \
+    {'I' : 1,
+    'V' : 5,
+    'X' : 10}
 
 class NumericalSystemsConverter:
     def __init__(self, src_system, target_system, number):
@@ -8,4 +12,16 @@ class NumericalSystemsConverter:
            self.rom_to_dec()
 
     def rom_to_dec(self):
-        self.target_number = 4
+        number_list = list(self.number)
+        self.target_number = rom_to_dec_dict[number_list[-1]]
+        self.previous_number = rom_to_dec_dict[number_list[-1]]
+        for i in reversed(number_list[:-1]):
+            if self.previous_number <= rom_to_dec_dict[i]:
+                self.target_number += rom_to_dec_dict[i]
+            else:
+                self.target_number -= rom_to_dec_dict[i]
+            self.previous_number = rom_to_dec_dict[i]
+
+
+if __name__ == '__main__':
+    NumericalSystemsConverter('ROM', 'DEC', 'XXIV')
